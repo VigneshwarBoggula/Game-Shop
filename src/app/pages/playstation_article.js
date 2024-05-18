f86e752ee469c22ea37f18d32ba1a37810e37aaa
Vigneshwar Boggula <vboggula@uci.edu>
2024-05-18 14:24:22 -0700

    navigation testing

--- src/app/pages/playstation_article.js
+++ src/app/pages/playstation_article.js
@@ -4,33 +4,6 @@ import '../styles/ps5.css';
 const PlayStation5Review = () => {
     return (
         <div>
-            <nav>
-                <div className="logo">
-                    <a href="../home/index.html">GameShop</a>
-                </div>
-                <div className="nav-links">
-                    <a href="../home/index.html">Home</a>
-                    <div className="dropdown">
-                        <a href="#" className="dropbtn">Products</a>
-                        <div className="dropdown-content">
-                            <a href="../products/videogames.html">Video Games</a>
-                            <a href="../products/consoles.html">Consoles</a>
-                            <a href="../products/accessories.html">Accessories</a>
-                        </div>
-                    </div>
-                    <a href="../press/press.html">Press & Reviews</a>
-                    <a href="../contact/contact.html">Contact</a>
-                </div>
-                <div className="icons">
-                    <img src="../../../public/search_icon.png" alt="Search" />
-                    <a href="../cart/cart.html">
-                        <img src="../../../public/cart_icon.png" alt="Cart" />
-                    </a>
-                    <a href="../account/account.html">
-                        <img src="../../../public/account_icon.png" alt="Account" />
-                    </a>
-                </div>
-            </nav>
             <header>
                 <h1>PlayStation 5: Game Changer or Disappointment?</h1>
             </header>
