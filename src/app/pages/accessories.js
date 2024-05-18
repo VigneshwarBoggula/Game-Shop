ba6d926443346ed72cb2213309e08c531fb2258c
Vigneshwar Boggula <vboggula@uci.edu>
2024-05-18 15:26:04 -0700

    accessories page

--- src/app/pages/accessories.js
+++ src/app/pages/accessories.js
@@ -1,12 +1,34 @@
 'use client';
 
 import React from 'react';
+import '../styles/products.css';
 
 const Accessories = () => {
   return (
-    <div>
-      <h1>Accessories Page</h1>
-      <p>Welcome to the accessories page!</p>
+    <div className="accessories-page">
+      <h1>Accessories</h1>
+      <div className="buttons">
+        <button className="filter-btn">Filter</button>
+        <button className="sort-btn">Sort</button>
+      </div>
+      <div className="product-boxes-section">
+        {/* Product Boxes */}
+        <div className="product-box">
+          <img src="/headsets.jpg" alt="Headsets" />
+          <h3>Headsets</h3>
+          <a href="#">Browse Now</a>
+        </div>
+        <div className="product-box">
+          <img src="/controllers.jpg" alt="Controllers" />
+          <h3>Controllers</h3>
+          <a href="#">Browse Now</a>
+        </div>
+        <div className="product-box">
+          <img src="/chargers.jpg" alt="Chargers" />
+          <h3>Chargers</h3>
+          <a href="#">Browse Now</a>
+        </div>
+      </div>
     </div>
   );
 };
