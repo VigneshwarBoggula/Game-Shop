0950438e3461c1d4af7bf55c00d478f588b858f1
Vigneshwar Boggula <vboggula@uci.edu>
2024-05-18 13:20:11 -0700

    react router dom

--- src/app/components/navigationbar/navigationbar.js
+++ src/app/components/navigationbar/navigationbar.js
@@ -1,23 +1,24 @@
-"use client"
+'use client';
 
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './navigationbar.css';
 
-const NavigationBar = ({ onSelectPage }) => {
+const NavigationBar = () => {
   return (
     <nav className="navigation-bar">
       <ul className="nav-list">
-        <li className="nav-item"><a href="#" onClick={() => onSelectPage('home')}>Home</a></li>
-        <li className="nav-item"><a href="#" onClick={() => onSelectPage('videogames')}>VideoGames</a></li>
-        <li className="nav-item"><a href="#" onClick={() => onSelectPage('consoles')}>Consoles</a></li>
-        <li className="nav-item"><a href="#" onClick={() => onSelectPage('accessories')}>Accessories</a></li>
-        <li className="nav-item"><a href="#" onClick={() => onSelectPage('press')}>Press & Reviews</a></li>
-        <li className="nav-item"><a href="#" onClick={() => onSelectPage('playstation_article')}>PS Article</a></li>
-        <li className="nav-item"><a href="#" onClick={() => onSelectPage('mw3_article')}>MW3 Article</a></li>
-        <li className="nav-item"><a href="#" onClick={() => onSelectPage('contact')}>Contact</a></li>
-        <li className="nav-item"><a href="#" onClick={() => onSelectPage('cart')}>Cart</a></li>
-        <li className="nav-item"><a href="#" onClick={() => onSelectPage('account')}>Account</a></li>
-        <li className="nav-item"><a href="#" onClick={() => onSelectPage('order')}>Order History</a></li>
+        <li className="nav-item"><Link to="/">Home</Link></li>
+        <li className="nav-item"><Link to="/products">Video Games</Link></li>
+        <li className="nav-item"><Link to="/about">Consoles</Link></li>
+        <li className="nav-item"><Link to="/contact">Accessories</Link></li>
+        <li className="nav-item"><Link to="/press">Press & Reviews</Link></li>
+        <li className="nav-item"><Link to="/playstation_article">PS Article</Link></li>
+        <li className="nav-item"><Link to="/mw3_article">MW3 Article</Link></li>
+        <li className="nav-item"><Link to="/contact">Contact</Link></li>
+        <li className="nav-item"><Link to="/cart">Cart</Link></li>
+        <li className="nav-item"><Link to="/account">Account</Link></li>
+        <li className="nav-item"><Link to="/order">Order History</Link></li>
       </ul>
     </nav>
   );
