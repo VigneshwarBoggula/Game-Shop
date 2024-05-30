bbbd21feefb00905ecafa21d64c8ea399be5a14a
Vigneshwar Boggula <vboggula@uci.edu>
2024-05-29 19:38:09 -0700

    setting bar component

---
+++ frontend/src/components/settingsbar/settingsbar.js
@@ -0,0 +1,23 @@
+'use client';
+
+import React from 'react';
+import Link from 'next/link';
+import './settingsbar.css';
+
+const SettingsBar = () => {
+    return (
+        <div className="container">
+            <aside>
+                <ul>
+                    <li><Link href="/account">Account Settings</Link></li>
+                    <li><Link href="/order">Order History</Link></li>
+                    <li><Link href="/edit_products">Product Inventory</Link></li>
+                    <li><Link href="/edit_articles">Article Catalog</Link></li>
+                    <li><Link href="/view_contacts">View Inqueries</Link></li>
+                </ul>
+            </aside>
+        </div>
+    );
+};
+
+export default SettingsBar;
