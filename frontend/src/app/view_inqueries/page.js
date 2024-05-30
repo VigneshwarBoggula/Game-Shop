988e4cbafd42b6e6e48484bec69b0802b307b0cf
Vigneshwar Boggula <vboggula@uci.edu>
2024-05-30 12:17:29 -0700

    rename view_contacts to view_inqueries

---
+++ frontend/src/app/view_inqueries/page.js
@@ -0,0 +1,25 @@
+"use client";
+
+import { useState, useEffect } from 'react';
+import SettingsBar from '../../components/settingsbar/settingsbar';
+import Link from 'next/link';
+import axios from 'axios';
+
+const ContactsPage = () => {
+  return (
+    <div className="container">
+        <aside>
+            <ul>
+                <li><Link href="/account">Account Settings</Link></li>
+                <li><Link href="/order">Order History</Link></li>
+                <li><Link href="/edit_products">Product Inventory</Link></li>
+                <li><Link href="/edit_articles">Article Catalog</Link></li>
+                <li><Link href="/view_inqueries">View Inqueries</Link></li>
+            </ul>
+        </aside>
+        <p>View All Inqueries Here</p>
+    </div>
+  );
+};
+
+export default ContactsPage;
