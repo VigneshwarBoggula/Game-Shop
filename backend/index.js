9bd69e75d6cea63c6416542f5ce92ba23e6bc477
Vigneshwar Boggula <vboggula@uci.edu>
2024-06-05 01:02:40 -0700

    product endpoints

--- backend/index.js
+++ backend/index.js
@@ -97,7 +97,126 @@ app.delete('/users/:id', async (req, res) => {
 });
 
 /* Product */
-// TODO
+// get all products
+app.get('/products', async (req, res) => {
+  try {
+    const products = await prisma.product.findMany();
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// get product by id
+app.get('/products/:id', async (req, res) => {
+  try {
+    const product = await prisma.product.findUnique({
+      where: {
+        id: Number(req.params.id),
+      },
+    });
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// get products by category - videogames
+app.get('/products/category/videogames', async (req, res) => {
+  try {
+    const products = await prisma.product.findMany({
+      where: {
+        category: 'videogames',
+      },
+    });
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// get products by category - consoles
+app.get('/products/category/consoles', async (req, res) => {
+  try {
+    const products = await prisma.product.findMany({
+      where: {
+        category: 'consoles',
+      },
+    });
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// get products by category - accessories
+app.get('/products/category/accessories', async (req, res) => {
+  try {
+    const products = await prisma.product.findMany({
+      where: {
+        category: 'accessories',
+      },
+    });
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// create product
+app.post('/products', async (req, res) => {
+  try {
+    const product = await prisma.product.create({
+      data: {
+        name: req.body.name,
+        image: req.body.image,
+        price: req.body.price,
+        description: req.body.description,
+        stock: req.body.stock,
+        category: req.body.category
+      },
+    });
+    res.status(201).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// update product
+app.put('/products/:id', async (req, res) => {
+  try {
+    const product = await prisma.product.update({
+      where: {
+        id: Number(req.params.id),
+      },
+      data: {
+        name: req.body.name,
+        image: req.body.image,
+        price: req.body.price,
+        description: req.body.description,
+        stock: req.body.stock,
+        category: req.body.category
+      },
+    });
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// delete product
+app.delete('/products/:id', async (req, res) => {
+  try {
+    const product = await prisma.product.delete({
+      where: {
+        id: Number(req.params.id),
+      },
+    });
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 /* Article */
 // TODO
