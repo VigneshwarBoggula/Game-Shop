190729b102a382061906f52590cab6d19df4bcbd
Vigneshwar Boggula <vboggula@uci.edu>
2024-06-05 01:09:41 -0700

    props for Product in db

--- frontend/src/models/productbox/productbox.js
+++ frontend/src/models/productbox/productbox.js
@@ -1,6 +1,7 @@
 "use client";
 
 import React from 'react';
+import PropTypes from 'prop-types';
 import './productbox.css';
 
 const ProductBox = ({ id, title, price, image, description, quantity, addToCart }) => {
@@ -19,6 +20,16 @@ const ProductBox = ({ id, title, price, image, description, quantity, addToCart 
       </div>
     </div>
   );
+};
+
+ProductBox.propTypes = {
+  id: PropTypes.number.isRequired,
+  image: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  price: PropTypes.string.isRequired,
+  quantity: PropTypes.number.isRequired,
+  description: PropTypes.string.isRequired,
+  addToCart: PropTypes.func.isRequired,
 };
 
 export default ProductBox;
