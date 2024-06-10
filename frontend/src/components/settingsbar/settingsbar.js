7798d45a1db2e70d73924dff1aa347792755d1fa
Vigneshwar Boggula <vboggula@uci.edu>
2024-06-10 01:29:53 -0700

    removed page

--- frontend/src/components/settingsbar/settingsbar.js
+++ frontend/src/components/settingsbar/settingsbar.js
@@ -12,7 +12,6 @@ const SettingsBar = () => {
                     <li><Link href="/account">Account Settings</Link></li>
                     <li><Link href="/order">Order History</Link></li>
                     <li><Link href="/edit_products">Manage Products</Link></li>
-                    <li><Link href="/edit_orders">Manage Articles</Link></li>
                 </ul>
             </aside>
         </div>
