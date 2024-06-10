5a5cd024bf7c4c34396d07e67adde944e47202c2
Layth Al Nabulsi <laythnabulsi@gmail.com>
2024-06-10 00:44:29 -0700

    changed title

--- frontend/src/components/settingsbar/settingsbar.js
+++ frontend/src/components/settingsbar/settingsbar.js
@@ -12,7 +12,7 @@ const SettingsBar = () => {
                     <li><Link href="/account">Account Settings</Link></li>
                     <li><Link href="/order">Order History</Link></li>
                     <li><Link href="/edit_products">Manage Products</Link></li>
-                    <li><Link href="/edit_orders">Manage Orders</Link></li>
+                    <li><Link href="/edit_orders">Manage Articles</Link></li>
                 </ul>
             </aside>
         </div>
